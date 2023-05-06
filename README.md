498d7b5b8b53492f2edc2a2f79948e35c4b29b86
Ivan Clay <ivan.clay.moura@gmail.com>
2023-05-05 23:01:22 -0300

    feat(advanced): voice to text js advanced version.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Programming Playground
 
+- [Speech Recognition in JS](https://ivanclay.github.io/playground/speech-recognition)
 - [Text File to Speech in JS](https://ivanclay.github.io/playground/text-file-to-speech-js)
 - [Text to Speech in JS](https://ivanclay.github.io/playground/text-to-speech-js)
 - [Letter and Words Counter](https://ivanclay.github.io/playground/letter-words-counter)
---
+++ speech-recognition/js/script.js
@@ -0,0 +1,39 @@
+let textarea = document.querySelector('#textarea');
+let voices = document.querySelector('#voices');
+let button = document.querySelector('#button');
+let clear = document.querySelector('#clear');
+let selectedVoice = 0;
+let voicesList = [];
+let listening = false;
+
+const recognition = createRecognition();
+
+clear.addEventListener('click', e => {
+    textarea.innerHTML = '';
+});
+
+button.addEventListener('click', e => {
+   
+    if(!recognition) return;
+
+    listening ? recognition.stop() : recognition.start();
+    button.innerHTML = listening ? 'Click and Speak' : 'Click and Read';
+});
+
+function createRecognition(){
+    const speechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
+    const recognition = speechRecognition !== undefined ? new speechRecognition() : null;
+
+    if(!recognition){
+        textarea.innerHTML = "Speech recognition not found!";
+        return null;
+    }
+
+    recognition.lang = "pt_BR";
+    recognition.onstart = () => listening = true;
+    recognition.onend = () => listening = false;
+    recognition.onerror = e => console.log('error', e);
+    recognition.onresult = e => textarea.innerHTML = e.results[0][0].transcript;
+
+    return recognition;
+}
