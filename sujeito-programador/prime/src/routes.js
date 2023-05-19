72cd27dbee85241f54334c39ace32b1b201df7de
Ivan Clay <ivan.clay.moura@gmail.com>
2023-05-19 07:17:22 -0300

    feat: create routes and header component

--- sujeito-programador/prime/src/routes.js
+++ sujeito-programador/prime/src/routes.js
@@ -3,10 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //PAGES
 import Home from './pages/Home';
 import Filme from './pages/Filme';
+//COMPONENTS
+import Header from './Components/Header';
 
 function RoutesApp(){
     return(
         <BrowserRouter>
+        <Header/>
         <Routes>
             <Route path='/' element={ <Home/> }/>
             <Route path='/filme/:id' element={ <Filme/> }/>
