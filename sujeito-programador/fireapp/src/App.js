b567d5768c5ce5a216ec955663047c81d1f7e7e5
Ivan Clay <ivan.clay.moura@gmail.com>
2023-05-21 10:01:03 -0300

    feat: firebase app

---
+++ sujeito-programador/fireapp/src/App.js
@@ -0,0 +1,11 @@
+import { db } from './firebaseConnection';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>REACT FIREBASE</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ sujeito-programador/fireapp/src/firebaseConnection.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAf1-cOPhL-H997xN504rBLrihJABnyndA",
+    authDomain: "colegio-1b43c.firebaseapp.com",
+    projectId: "colegio-1b43c",
+    storageBucket: "colegio-1b43c.appspot.com",
+    messagingSenderId: "686413222236",
+    appId: "1:686413222236:web:72f67b09b2c22e97c3b199",
+    measurementId: "G-WGJF7NJ6HG"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getFirestore(firebaseApp);
+
+  export { db };
