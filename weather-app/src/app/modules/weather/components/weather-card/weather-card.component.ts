d73ec0d5b93e3732139a93e8d8481214bde2ce2d
Ivan Clay <ivan.clay.moura@gmail.com>
2024-01-06 14:31:19 -0300

    feat: weather data card input

--- weather-app/src/app/modules/weather/components/weather-card/weather-card.component.ts
+++ weather-app/src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WeatherData } from 'src/app/models/interfaces/weather-data';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: ['./weather-card.component.scss']
 })
-export class WeatherCardComponent {
+export class WeatherCardComponent implements OnInit {
+
+@Input() weatherDataInput!: WeatherData;
+
+ngOnInit(): void {
+  console.log('Dados do Pai', this.weatherDataInput);
+}
 
 }
